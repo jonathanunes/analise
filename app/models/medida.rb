b146b07ea9dc1b23761bb5396db3b00b62104e6c
igordcsouza <igordcsouza@me.com>
2015-06-05 09:56:17 -0300

    add rescue quando opara capturar erros quando os links estiverem errados.

--- app/models/medida.rb
+++ app/models/medida.rb
@@ -15,23 +15,34 @@ class Medida < ActiveRecord::Base
   private
 
   def getTwitter(twitter)
-    source = open("https://twitter.com/#{twitter}").read
-    a = source.split('<a class="ProfileNav-stat ProfileNav-stat--link u-borderUserColor u-textCenter js-tooltip js-nav u-textUserColor" title="')
-    a = source.split('class="ProfileNav-item ProfileNav-item--followers"')
-    self.twitter = a[1].split('title')[1].split(' ')[0].gsub!('=','').gsub!('"','').to_f
-
+    begin
+      source = open("https://twitter.com/#{twitter}").read
+      a = source.split('<a class="ProfileNav-stat ProfileNav-stat--link u-borderUserColor u-textCenter js-tooltip js-nav u-textUserColor" title="')
+      a = source.split('class="ProfileNav-item ProfileNav-item--followers"')
+      self.twitter = a[1].split('title')[1].split(' ')[0].gsub!('=','').gsub!('"','').to_f
+    rescue => ex
+      logger.error ex.message
+    end
   end
 
   def getAlexa(url)
+    begin
     source = open("http://www.alexa.com/siteinfo/#{url}").read
     self.alexa = source.split('metrics-data align-vmiddle')[1].split('>')[2].split('<')[0].gsub!(',','.').gsub!("\n",'').gsub!(' ','')
+    rescue => ex
+      logger.error ex.message
+    end
   end
 
   def getFacebook(facebook)
-    source = open("https://graph.facebook.com/#{facebook}/").read
-    code = JSON.parse(source)
-    getFacebookLikes(code)
-    getFacebookTalk(code)
+    begin
+      source = open("https://graph.facebook.com/#{facebook}/").read
+      code = JSON.parse(source)
+      getFacebookLikes(code)
+      getFacebookTalk(code)
+    rescue => ex
+      logger.error ex.message
+    end
   end
 
   def getFacebookLikes(code)
